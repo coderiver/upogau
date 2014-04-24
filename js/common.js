fb5e898154967e19e54b0c3637028c3b112e6a07
Denis Pismenny <denis.pismenny@gmail.com>
2014-04-24 13:37:13 +0300

    add blog slider

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+
 	console.log('im ok');
+
+	$('.js-blogs').each(function(){
+		var p = this.parentNode;
+		$(this).cycle({
+			fx: "carousel",
+			// carouselVertical: true,
+			autoHeight: "container",
+			timeout:  0,
+			carouselVisible: 1,
+			slideActiveClass: "is-active",
+			pagerActiveClass: "is-active",
+			disabledClass: "is-disabled",
+			slideClass: "blog__slide",
+			allowWrap: "true",
+			carouselFluid: "true",
+			slides: $('.blog__slide', p),
+			prev:   $('.blog__prev', p),
+			next:   $('.blog__next', p),
+		});
+
+	});
+
 });
--- js/init.js
+++ js/init.js
@@ -1,1 +1,7 @@
-head.load("js/jquery.js", "js/common.js");
+head.load(
+	"js/jquery.js",
+	"http://cdnjs.cloudflare.com/ajax/libs/jquery.cycle2/20140216/jquery.cycle2.min.js",
+	"http://malsup.github.io/jquery.cycle2.carousel.js",
+	"http://malsup.github.io/jquery.cycle2.scrollVert.js",
+	"js/common.js"
+);
